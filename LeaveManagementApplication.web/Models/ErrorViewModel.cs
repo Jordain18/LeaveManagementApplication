4a166357cbf05b36508fd21a640c865ce54e867d
JordainCampbell <60863792+Jordain18@users.noreply.github.com>
2025-10-26 10:07:37 -0500

    first commit

---
+++ LeaveManagementApplication.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeaveManagementApplication.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
